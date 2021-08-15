6d2d5a5b77d65f84c1c0c66cda8abdd1e75acc4e
Vuyo <hlumi68@gmail.com>
2021-08-15 12:23:04 +0200

    test

---
+++ src/AuthFiles/_test_/Login.test.js
@@ -0,0 +1,14 @@
+  
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom"
+import Login from '../Login';
+
+test('Renders without error', () => {
+    render(
+        <BrowserRouter>
+            <Login/>
+        </BrowserRouter>
+    );
+    const btn = screen.getByText(/Login/i);
+    expect(btn).toBeInTheDocument();
+});
