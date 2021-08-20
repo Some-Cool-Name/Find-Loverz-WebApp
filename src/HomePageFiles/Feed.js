86fe22606d9205ac7c3c9060b45cd0e936695991
ThandoAmatullah <1870175@students.wits.ac.za>
2021-08-20 12:05:21 +0200

    Added Hello User

--- src/HomePageFiles/Feed.js
+++ src/HomePageFiles/Feed.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { globalStyles } from '../globalStyles';
 
 function Feed() {
+    const location = useLocation();
+    console.log("hello ", location.state);
+
+    if(location.state === undefined){
+        return null;
+    }
+    else{
+        return(
+            <div>
+                <h1> Hello {location.state}</h1>
+                <Link to="/" style={globalStyles.button}>Logout</Link>
+            </div>
+        )
+    }
     return (
         <div>
             <h1>Feed</h1>
