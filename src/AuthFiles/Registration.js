a75b0013fd53717b02710768b4488718cf4f9346
Ntiyis <69907364+Ntiyis@users.noreply.github.com>
2021-09-04 18:50:54 +0200

    registration design

--- src/AuthFiles/Registration.js
+++ src/AuthFiles/Registration.js
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
 import { useHistory } from "react-router-dom";
-import "../index.css";
+import { globalStyles } from "../globalStyles";
+import "./Registration.css";
 
 function Registration() {
   let history = useHistory();
@@ -35,42 +36,67 @@ function Registration() {
   };
 
   return (
-    <div className="registration">
-      <h1>Register</h1>
+    <div className = "container">
+       <div className='header'>
+                <h3 className='appName'>find loverz</h3>
+             </div>
+      <div className="registration">
       <form className="form" onSubmit={registerUser}>
+      <h1>Register</h1>
+      <hr width="100px;"  size="8"></hr>
         <div className="form-element">
-          <label htmlFor="username"> Username</label>
+          <label className = "fieldDescription" htmlFor="username"> Username</label>
           <input
+          className = 'inputBox'
             id="username"
+            required
+            type ="text"
+            placeholder = "Username..."
+            style = {globalStyles.inputStyle}
             onChange={(e) => setUsername(e.target.value)}
           ></input>
         </div>
 
         <div className="form-element">
-          <label htmlFor="password"> Password</label>
+          <label className = "fieldDescription" htmlFor="password"> Password</label>
           <input
+          className = 'inputBox'
             id="password"
+            required
             type="password"
+            placeholder = "Password..."
+            style = {globalStyles.inputStyle}
             onChange={(e) => setPassword(e.target.value)}
           ></input>
         </div>
 
         <div className="form-element">
-          <label htmlFor="confirmPassword"> Confirm password</label>
+          <label className = "fieldDescription" htmlFor="confirmPassword"> Confirm password</label>
           <input
+          className = 'inputBox'
             id="confirmPassword"
+            required
+            placeholder = "Confirm Password"
+            style = {globalStyles.inputStyle}
             type="password"
             onChange={(e) => setConfirm(e.target.value)}
           ></input>
         </div>
 
         <div className="form-element">
-          <label htmlFor="name"> Name</label>
-          <input id="name" onChange={(e) => setName(e.target.value)}></input>
+          <label className = "fieldDescription" htmlFor="name"> Name</label>
+          <input className = 'inputBox' 
+          id="name" 
+          required
+          type = "text"
+          placeholder ="Name"
+          style = {globalStyles.inputStyle}
+          onChange={(e) => setName(e.target.value)}
+          ></input>
         </div>
 
         <div className="form-element">
-          <label htmlFor="gender"> Gender</label>
+          <label className = "fieldDescription" htmlFor="gender"> Gender</label>
           <select id="gender" onChange={(e) => setGender(e.target.value)}>
             <option>Select an Option</option>
             <option>Male</option>
@@ -80,15 +106,20 @@ function Registration() {
         </div>
 
         <div className="form-element">
-          <label htmlFor="birthday"> Birthday</label>
+          <label className = "fieldDescription" htmlFor="birthday"> Birthday</label>
           <input
+          className = 'inputBox'
             id="birthday"
+            required
+            type = "text"
+            placeholder = "Birthday"
+            style = {globalStyles.inputStyle}
             onChange={(e) => setBirthday(e.target.value)}
           ></input>
         </div>
 
         <div className="form-element">
-          <label htmlFor="sexuality"> Sexuality</label>
+          <label className = "fieldDescription" htmlFor="sexuality"> Sexuality</label>
           <select id="sexuality" onChange={(e) => setSexuality(e.target.value)}>
             <option>Select an Option</option>
             <option>Male</option>
@@ -98,16 +129,23 @@ function Registration() {
         </div>
 
         <div className="form-element">
-          <label htmlFor="location"> Location</label>
+          <label className = "fieldDescription" htmlFor="location"> Location</label>
           <input
+          className = 'inputBox'
             id="location"
+            required
+            type = "text"
+            placeholder = "Location"
+            style = {globalStyles.inputStyle}
             onChange={(e) => setLocation(e.target.value)}
           ></input>
         </div>
 
-        <input id="register-button" type="submit" value="Register" />
+        <input className = "button" id="register-button" style = {globalStyles.button} type="submit" value="Register" />
       </form>
+    </div> 
     </div>
+    
   );
 }
 // link =https://lamp.ms.wits.ac.za/home/s1851427/webb.php?username=br4&password=brbr&name=vhugala&gender=male&birthday=2000-02-20&sexuality=female&location=braam
