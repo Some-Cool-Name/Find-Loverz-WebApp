fc4d86b81c61f0005d2ca9a6eaabfa33c3cca1fe
ThandoAmatullah <1870175@students.wits.ac.za>
2021-10-11 09:03:55 +0200

    added edit profile

--- src/HomePageFiles/NavBar.js
+++ src/HomePageFiles/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { deleteFromStorage } from '../HelperClasses/StorageHandler';
 
-export default function NavBar({ setUser }) {
+export default function NavBar({ setUser, user}) {
     let history = useHistory();
 
     // handles user logout
@@ -13,10 +13,25 @@ export default function NavBar({ setUser }) {
     }
 
     return (        
-        <div>
-            <Link to="/feed" style={{marginRight: 5}} > <b>Feed</b> </Link>
-            <Link to="/matches" style={{marginRight: 5}} > <b>Chat</b> </Link>
-            <Link to="/" onClick={handleLogout} > <b>Logout</b> </Link>
-        </div>
+        <navbar>
+            <div className="left-nav">
+                <h1 id="nav-app-name">Find Loverz</h1>
+                <div className="profile-container">
+                    <div className = "match-profile-picture"><img src = {user[0]?.profile_picture} alt="" /> </div>
+                        <div className="profile-name">
+                            <p id="feed-username">{user[0]?.username}</p>                          
+                        </div>
+                </div>
+            </div>
+
+            <div className="right-nav">
+                <Link to="/feed" style={{marginRight: 5}} ><p>Home</p></Link>
+                <Link to="/matches" style={{marginRight: 5}} ><p>Chats</p></Link>
+                <Link to="/profile" style={{marginRight: 5}} ><p>Profile</p></Link>
+                <input type="text" className="nav-search-bar" id="nav-search-bar" placeholder="Search"></input>
+                <div className="search-button"><i class="uil uil-search"></i></div>
+                <Link to="/" onClick={handleLogout} ><p>Logout</p></Link>
+            </div>
+        </navbar>
     )
 }
