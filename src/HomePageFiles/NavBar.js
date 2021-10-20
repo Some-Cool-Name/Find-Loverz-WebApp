15cd0ef0b8b12997a289c7734971b02ba65ce89b
othnielNgobeni <81512981+othnielNgobeni@users.noreply.github.com>
2021-10-20 12:19:07 +0200

    ui ot done

--- src/HomePageFiles/NavBar.js
+++ src/HomePageFiles/NavBar.js
@@ -33,24 +33,5 @@ export default function NavBar({ setUser, user}) {
             
         </div>
 
-        // <navbar>
-        //     <div className="left-nav">
-        //         <h1 id="nav-app-name">Find Loverz</h1>
-        //         <div className="profile-container">
-        //             <div className = "match-profile-picture"><img src = {user[0].profile_picture} alt="" /> </div>
-        //                 <div className="profile-name">
-        //                     <p id="feed-username">{user[0].username}</p>                          
-        //                 </div>
-        //         </div>
-        //     </div>
-
-        //     <div className="right-nav">
-        //         <Link to="/feed" style={{marginRight: 5}} ><p>Home</p></Link>
-        //         <Link to="/matches" style={{marginRight: 5}} ><p>Chats</p></Link>
-        //         <input type="text" className="nav-search-bar" id="nav-search-bar" placeholder="Search"></input>
-        //         <div className="search-button"><i class="uil uil-search"></i></div>
-        //         <Link to="/" onClick={handleLogout} ><p>Logout</p></Link>
-        //     </div>
-        // </navbar>
     )
 }
