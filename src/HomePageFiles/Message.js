45cec0cac2080c656fb754b2cf4f8f39948b9670
Muda-u <vhugala.m00@gmail.com>
2021-10-21 11:16:11 +0200

    image send

--- src/HomePageFiles/Message.js
+++ src/HomePageFiles/Message.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Chat.css';
 
 
-function Message({ text, isMe }) {
+function Message({ text, isMe, url }) {
 
     if(isMe){
         return (
             <div className="chat-outgoing">
                 <p className="chat-message">{text}</p>
+                {url !== "" &&
+                <img src={url}></img>
+                
+                
+                }
                 {/* <p className="chat-time">20:25</p> */}
             </div>
         ) 
