93698b3101a9b286b9879ec450709ddc5614bdab
Vuyo <hlumi68@gmail.com>
2021-11-01 12:34:29 +0200

    shaking bug

--- src/HomePageFiles/FeedCard.js
+++ src/HomePageFiles/FeedCard.js
@@ -2,29 +2,27 @@ import React from 'react';
 
 function FeedCard({ userAccount, fetchUsers, user }) {
 
-    const swipeRight = (likedUser) => {
-        const like = async (e)=>{
-            e.preventDefault()
-            const results = await fetch("https://lamp.ms.wits.ac.za/home/s1851427/WDALikeUser.php?"+
-            `likerUsername=${user[0].username}&likeeUsername=${likedUser.E_mail}`
-            )
-            fetchUsers();
-        }
-        const temp = like;
-        return temp;
-    }
+  const like = async (likedUser) => {
+    await fetch("https://lamp.ms.wits.ac.za/home/s1851427/WDALikeUser.php?"+
+    `likerUsername=${user[0].username}&likeeUsername=${likedUser.E_mail}`
+    )
+    fetchUsers();
+  } 
+  
+  const reject = async (likedUser) => {
+    await fetch("https://lamp.ms.wits.ac.za/home/s1851427/WDARejectUser.php?"+
+    `rejectorUsername=${user[0].username}&rejecteeUsername=${likedUser.E_mail}`
+    )
+    fetchUsers();
+  }
+
+  const swipeRight = async (likedUser) => {
+    await like(likedUser);
+  }
     
-    const swipeLeft = (likedUser) => {
-        const reject = async (e)=>{
-            e.preventDefault()
-            const results = await fetch("https://lamp.ms.wits.ac.za/home/s1851427/WDARejectUser.php?"+
-            `rejectorUsername=${user[0].username}&rejecteeUsername=${likedUser.E_mail}`
-            )
-            fetchUsers();
-        } 
-        const temp = reject;
-        return temp;
-    }
+  const swipeLeft = async (likedUser) => {
+     await reject(likedUser);
+  }
 
 
     return (
@@ -44,8 +42,8 @@ function FeedCard({ userAccount, fetchUsers, user }) {
           </div>
         </div>
         <div className="button-container">
-          <div onClick={swipeRight(userAccount)} id="yes-button"><i className="uil uil-check"></i></div>
-          <div onClick={swipeLeft(userAccount)} id="no-button" ><i className="uil uil-times"></i></div>
+          <div onClick={() => swipeRight(userAccount)} id="yes-button"><i className="uil uil-check"></i></div>
+          <div onClick={() => swipeLeft(userAccount)} id="no-button" ><i className="uil uil-times"></i></div>
         </div>
       </div>
     )
