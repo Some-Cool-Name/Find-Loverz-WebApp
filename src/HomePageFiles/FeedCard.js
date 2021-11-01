a6b680146cef79027029f0ab7465a7d5a2e05b9b
othnielNgobeni <81512981+othnielNgobeni@users.noreply.github.com>
2021-11-01 04:49:39 +0200

    ui updated

--- src/HomePageFiles/FeedCard.js
+++ src/HomePageFiles/FeedCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ImLocation2 } from "react-icons/im";
 
 function FeedCard({ userAccount, fetchUsers, user }) {
 
@@ -32,12 +33,16 @@ function FeedCard({ userAccount, fetchUsers, user }) {
         <img className="card-image" src={userAccount.Profile_Picture} ></img>
           <div className="card-id">
             <p id="feed-name">{userAccount.Full_Name }</p>
-        
           </div>
-        <div className="card-interests">
-        {userAccount.Interest_1 +", "+ userAccount.Interest_2 +", "+ userAccount.Interest_3 +", "+ userAccount.Interest_4 +", "+ userAccount.Interest_5}
-        </div>
-     
+          <p title="location" className="location"><ImLocation2 style={{ color:"#808080", marginRight:"10px" }} />{userAccount.Location}</p>
+          <div className="interest2">
+                    <div className="interest-element2" title="intOne" style={{borderColor: "#12c2e9", color:"#12c2e9" }} >{userAccount.Interest_1}</div>
+                    <div className="interest-element2" title="intTwo" style={{borderColor: "#c471ed", color:"#c471ed" }} >{userAccount.Interest_2}</div>
+                    <div className="interest-element2" title="intThree" style={{borderColor: "#fc32e8", color:"#fc32e8" }}>{userAccount.Interest_3}</div>
+                    <br />
+                    <div className="interest-element2" title="intFour" style={{borderColor: "#fc32e8", color:"#fc32e8" }}>{userAccount.Interest_4}</div>
+                    <div className="interest-element2" title="intFive" >{userAccount.Interest_5}</div>
+                </div>
         <div className="card-bio">
           <div>
             {userAccount.Bio }
